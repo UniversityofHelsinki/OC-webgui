b91db6422988ba39b57aa641f4f93f26144de9b2
Tuomas Siipola <siiptuo@kapsi.fi>
2016-05-19 20:17:04 +0300

    Add ESLint for JavaScript linting

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,25 @@
 angular.module('ocWebGui', ['ngResource', 'ui.router', 'templates'])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('home', {
                 url: '/home',
                 templateUrl: 'home.html',
-                controller: 'HomeCtrl'
+                controller: 'HomeController'
             })
             .state('screen', {
                 url: '/screen',
                 templateUrl: 'screen.html',
-                controller: 'ScreenCtrl'
+                controller: 'ScreenController'
             });
         $urlRouterProvider.otherwise('home');
-    }])
-    .controller('HomeCtrl', [function(){
-    }])
-    .controller('ScreenCtrl', ['$resource', '$interval', '$scope', function($resource, $interval, $scope){
+    })
+    .controller('HomeController', function() {
+    })
+    .controller('ScreenController', function($resource, $interval, $scope) {
         $scope.message = 'Hei maailma!';
 
         $interval(function() {
-        	$scope.users = $resource('http://localhost:3000/testis.json	').query();
+            $scope.users = $resource('http://localhost:3000/testis.json').query();
         }, 1000);
-    }]);
+    });
 
