b7d5868998c2aff378465877e4691a58465ba0dc
Luokkala Niklas M K <xnix@lx3-tike21.helsinki.fi>
2016-05-26 12:21:47 +0300

    Add static agent status screen

--- app/assets/javascripts/screen/screenController.js
+++ app/assets/javascripts/screen/screenController.js
@@ -10,7 +10,5 @@ angular.module('ocWebGui.screen', ['ui.router', 'ngResource'])
     .controller('ScreenController', function($resource, $interval, $scope) {
         $scope.message = 'Hei maailma!';
 
-        $interval(function() {
-            $scope.users = $resource('http://localhost:3000/testis.json').query();
-        }, 1000);
+	$scope.agents = [ {name: "eka", state: "syömässä"}, {name: "toka", state: "puhelimessa"}];
     });
