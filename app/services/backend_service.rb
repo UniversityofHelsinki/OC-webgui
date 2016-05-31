2303fa6e7f27a4e0fbd00bc91a3944394502dfc3
Tuomas Siipola <siiptuo@kapsi.fi>
2016-05-31 18:35:01 +0300

    Fix code style

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,27 +1,25 @@
 require 'savon'
 require 'dotenv'
 
-class BackendService 
-
-	def get_agent_online_state
-		Dotenv.load
-		password = ENV['PASSWORD']
-		client = Savon.client(
-		  wsdl: "https://ocsaas.elisa.fi/dsjicsewrxrtblidogief/reportwebservice/reportservice.asmx?wsdl",
-		  namespaces: {"xmlns:oc" => "http://elisa.ccreport.fi/"},
-		  follow_redirects: true,
-		  soap_header: {
-		    "oc:ServiceAuthHeader" => {
-		      "oc:WebServicePassword" => password
-		    }
-		  }
-		)
-		reply = client.call(:get_agent_online_state)
-		data = reply.body.dig(:get_agent_online_state_response, :get_agent_online_state_result, :array_of_string)
-		if not data
-			return []
-		end
-		data
-	end
-
+class BackendService
+  def get_agent_online_state
+    Dotenv.load
+    password = ENV['PASSWORD']
+    client = Savon.client(
+      wsdl: 'https://ocsaas.elisa.fi/dsjicsewrxrtblidogief/reportwebservice/reportservice.asmx?wsdl',
+      namespaces: { 'xmlns:oc' => 'http://elisa.ccreport.fi/' },
+      follow_redirects: true,
+      soap_header: {
+        'oc:ServiceAuthHeader' => {
+          'oc:WebServicePassword' => password
+        }
+      }
+    )
+    reply = client.call(:get_agent_online_state)
+    data = reply.body.dig(:get_agent_online_state_response, :get_agent_online_state_result, :array_of_string)
+    if not data
+      return []
+    end
+    data
+  end
 end
