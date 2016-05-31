2a50f90bdbbc7622a61ef1fbc6028c39a45dae6f
Tuomas Siipola <siiptuo@kapsi.fi>
2016-05-31 18:42:47 +0300

    Move Savon client creation on class initialization

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -2,20 +2,21 @@ require 'savon'
 require 'dotenv'
 
 class BackendService
-  def get_agent_online_state
-    Dotenv.load
-    password = ENV['PASSWORD']
-    client = Savon.client(
+  def initialize
+    @client = Savon.client(
       wsdl: 'https://ocsaas.elisa.fi/dsjicsewrxrtblidogief/reportwebservice/reportservice.asmx?wsdl',
       namespaces: { 'xmlns:oc' => 'http://elisa.ccreport.fi/' },
       follow_redirects: true,
       soap_header: {
         'oc:ServiceAuthHeader' => {
-          'oc:WebServicePassword' => password
+          'oc:WebServicePassword' => ENV['PASSWORD']
         }
       }
     )
-    reply = client.call(:get_agent_online_state)
+  end
+
+  def get_agent_online_state
+    reply = @client.call(:get_agent_online_state)
     data = reply.body.dig(:get_agent_online_state_response, :get_agent_online_state_result, :array_of_string)
     if not data
       return []
