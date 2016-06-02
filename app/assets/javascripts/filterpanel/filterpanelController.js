701b18790a75ac249552a3718673f03a83d69121
niklas <niklas@niklas.niklas>
2016-06-02 14:43:01 +0300

    filtering for status states

--- app/assets/javascripts/filterpanel/filterpanelController.js
+++ app/assets/javascripts/filterpanel/filterpanelController.js
@@ -8,7 +8,10 @@ angular.module('ocWebGui.filterpanel', ['ui.router', 'ngResource'])
             });
     })
     .controller('FilterpanelController', function FilterpanelController($scope, $rootScope) {
+      // kaikki vaihtoehdot
       $scope.teams = ["Hakijapalvelut", "Helpdesk", "Opiskelijaneuvonta", "OrangeContact 1", "Puhelinvaihde", "Uaf"];
+      $scope.states = ["BACKOFFICE", "<TELJENTÄ>", "Sisäänkirjaus", "TAUKO", "12648", "PUHELU (Sisään)", "PUHELU (Ulos)"];
+
       $scope.toggleSelection = function toggleSelection(team) {
         var idx = $rootScope.selection.indexOf(team);
         if (idx > -1) {
@@ -17,8 +20,19 @@ angular.module('ocWebGui.filterpanel', ['ui.router', 'ngResource'])
           $rootScope.selection.push(team);
         }
       };
+
+      $scope.toggleSelection2 = function toggleSelection2(state) {
+        var idx = $rootScope.selectedStates.indexOf(state);
+        if (idx > -1) {
+          $rootScope.selectedStates.splice(idx, 1);
+        } else {
+          $rootScope.selectedStates.push(state);
+        }
+      };
     })
 
   .run(function ($rootScope) {
+    // oletusvalinnat
     $rootScope.selection = ["Helpdesk"];
+    $rootScope.selectedStates = ["Sisäänkirjaus", "TAUKO", "PUHELU (Sisään)", "PUHELU (Ulos)"];
   });
