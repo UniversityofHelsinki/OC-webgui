4166344a528ce7986d981084e0e1ee5aad10f8de
niklas <niklas@niklas.niklas>
2016-06-02 13:48:51 +0300

    moved filter page to its own page

--- app/assets/javascripts/filterpanel/filterpanelController.js
+++ app/assets/javascripts/filterpanel/filterpanelController.js
@@ -4,9 +4,21 @@ angular.module('ocWebGui.filterpanel', ['ui.router', 'ngResource'])
             .state('filterpanel', {
                 url: '/filterpanel',
                 templateUrl: 'filterpanel/_filterpanel.html',
-                controller: 'filterpanelController'
+                controller: 'FilterpanelController'
             });
     })
-    .controller('filterpanelController', function($resource, $interval, $scope) {
-        $scope.message = 'Tilat-- ..';
-    });
+    .controller('FilterpanelController', function FilterpanelController($scope, $rootScope) {
+      $scope.teams = ["Hakijapalvelut", "Helpdesk", "Opiskelijaneuvonta", "OrangeContact 1", "Puhelinvaihde", "Uaf"];
+      $scope.toggleSelection = function toggleSelection(team) {
+        var idx = $rootScope.selection.indexOf(team);
+        if (idx > -1) {
+          $rootScope.selection.splice(idx, 1);
+        } else {
+          $rootScope.selection.push(team);
+        }
+      };
+    })
+
+  .run(function ($rootScope) {
+    $rootScope.selection = ["Helpdesk"];
+  });
--- app/assets/javascripts/screen/screenController.js
+++ app/assets/javascripts/screen/screenController.js
@@ -9,19 +9,5 @@ angular.module('ocWebGui.screen', ['ui.router', 'ngResource'])
     })
     .controller('ScreenController', function($resource, $interval, $scope) {
         $scope.message = 'Tilat';
-        $scope.teams = ["Hakijapalvelut", "Helpdesk", "Opiskelijaneuvonta", "OrangeContact 1", "Puhelinvaihde", "Uaf"];
         $scope.agents = $resource('agents.json').query();
-    })
-    .controller('SimpleArrayCtrl', ['$scope', function SimpleArrayCtrl($scope) {
-      $scope.teams = ["Hakijapalvelut", "Helpdesk", "Opiskelijaneuvonta", "OrangeContact 1", "Puhelinvaihde", "Uaf"];
-      $scope.selection = ["Helpdesk"];
-      $scope.toggleSelection = function toggleSelection(team) {
-        var idx = $scope.selection.indexOf(team);
-        if (idx > -1) {
-          $scope.selection.splice(idx, 1);
-        }
-        else {
-          $scope.selection.push(team);
-        }
-      };
-    }]);
+    });
