fbb4d3b09f3eb8731b7514789e3dc9f79921c598
Teemu Matvejeff <tamndrei567@gmail.com>
2016-06-07 16:29:43 +0300

    Preliminary agent status and color matching done. Preliminary implementation for running color tally in sidebar done.

--- app/assets/javascripts/screen/screenController.js
+++ app/assets/javascripts/screen/screenController.js
@@ -15,9 +15,55 @@ angular.module('ocWebGui.screen', ['ui.router', 'ngResource'])
     .controller('ScreenController', function($resource, $interval, $scope) {
         $scope.message = 'Tilat';
         
-        var agents = $resource('agents.json').query();
+        /*
+        var agents = $resource('agents.json', {}, {
+            'get': {
+                method: 'GET',
+                params: {},
+                isArray: true,
+                transformResponse: function(data, header){
+                    //Getting string data in response
+                    var jsonData = angular.fromJson(data);
+                    var notes = [];
+
+                    angular.forEach(jsonData, function(item){
+                        var note = new Note();
+                        note.noteTitle = item.title;  
+                        notes.push(note);
+                     });
+
+                    return notes;
+                                
+                }
+            }
+        }); */
         
-        console.log(agents);
-        
-        $scope.agents = agents;
+        // Agent status and colors classifications        
+        $scope.agents = $resource('agents.json').query(function() {
+            var agents = $scope.agents;
+            var green = 0;
+            var yellow = 0;
+            var red = 0;
+            
+            for(i=0; i< agents.length; i++) {
+                if(agents[i].status === "Sisäänkirjaus") {
+                    agents[i].color = "green";
+                    
+                } else if (agents[i].status === "TAUKO") {
+                    agents[i].color = "yellow";
+                    yellow++;
+                    
+                } else if(agents[i].status === "TÄYDENNÄ TÄHÄN!!!") {
+                    agents[i].color = "red";
+                    red++;
+                    
+                } else {
+                    agents[i].color = "black";
+                }
+            }
+            
+            $scope.green = green;
+            $scope.yellow = yellow;
+            $scope.red = red;
+        });
     });
