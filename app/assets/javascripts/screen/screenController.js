1ae9cd11aec2d7f7bc6af1c20d3d78738814d131
Teemu Matvejeff <tamndrei567@gmail.com>
2016-06-15 12:22:37 +0300

    Fix for unneeded double resouce call in screenController. Fix for disappearing status color in screen view.

--- app/assets/javascripts/screen/screenController.js
+++ app/assets/javascripts/screen/screenController.js
@@ -11,14 +11,40 @@ angular.module('ocWebGui.screen', ['ui.router', 'ngResource', 'ocWebGui.shared.t
         $scope.teams = shared.getTeams();
         $scope.states = shared.getStates();
         $scope.message = 'Tilat';
-        $scope.agents = [];
 
         function fetchData() {
             $resource('agents.json').query(function(agents) {
+                $scope.agents = [];
+                var green = 0;
+                var yellow = 0;
+                var red = 0;
+                
                 $scope.agents = agents.filter(function(agent) {
                     return (($scope.states[agent.status] == true)
                         && ($scope.teams[agent.team] == true));
                 });
+                
+                var agents = $scope.agents;
+            
+                for(var i=0; i< agents.length; i++) {
+                    if(agents[i].status === "Sisäänkirjaus") {
+                        agents[i].color = "green";
+                        green++;
+                                    
+                    } else if (agents[i].status === "JÄLKIKIRJAUS") {
+                        agents[i].color = "yellow";
+                        yellow++;
+                    
+                    } else {
+                        agents[i].color = "red";
+                        red++;
+                    
+                    }
+                }
+            
+                $scope.green = green;
+                $scope.yellow = yellow;
+                $scope.red = red;
             });
         }
 
@@ -29,31 +55,8 @@ angular.module('ocWebGui.screen', ['ui.router', 'ngResource', 'ocWebGui.shared.t
 
         fetchData();
         
-        // Agent status and colors classifications        
+        /* Agent status and colors classifications        
         $scope.agents = $resource('agents.json').query(function() {
-            var agents = $scope.agents;
-            var green = 0;
-            var yellow = 0;
-            var red = 0;
             
-            for(var i=0; i< agents.length; i++) {
-                if(agents[i].status === "Sisäänkirjaus") {
-                    agents[i].color = "green";
-                    green++;
-                    
-                } else if (agents[i].status === "JÄLKIKIRJAUS") {
-                    agents[i].color = "yellow";
-                    yellow++;
-                    
-                } else {
-                    agents[i].color = "red";
-                    red++;
-                    
-                }
-            }
-            
-            $scope.green = green;
-            $scope.yellow = yellow;
-            $scope.red = red;
-        });
+        }); */
     });
