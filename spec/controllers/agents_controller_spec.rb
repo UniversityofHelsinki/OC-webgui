fd8acb9b5e47fa9296c5d4765b09144d649fd287
nn <nn@nn.n>
2016-06-16 21:41:43 +0300

    bettered rspec test for agents controller

--- spec/controllers/agents_controller_spec.rb
+++ spec/controllers/agents_controller_spec.rb
@@ -1,9 +1,12 @@
 require "rails_helper"
+require 'pp'
 require_relative "../../app/controllers/agents_controller.rb"
 require_relative "../../app/controllers/application_controller.rb"
 require_relative "../../app/services/backend_service.rb"
 
 RSpec.describe AgentsController, type: :controller do
+  render_views
+
   it 'get agent online state should work' do
     expected = [{:agent_id=>"3300170",
                  :full_name=>"joku vaan",
@@ -23,11 +26,37 @@ RSpec.describe AgentsController, type: :controller do
                  :status=>"Sisäänkirjaus",
                  :time_in_status=>"13616"}]
 
+    # onko tässä ees järkee
+    expected_ilman_kaksoispisteitä_ja_name_eikä_full_name =
+               [{"agent_id"=>3300170,
+                 "name"=>"joku vaan",
+                 "team"=>"Hakijapalvelut",
+                 "status"=>"Sisäänkirjaus",
+                 "time_in_status"=>5984},
+
+                {"agent_id"=>2200044,
+                 "name"=>"testaus ukko",
+                 "team"=>"Opiskelijaneuvonta",
+                 "status"=>"JÄLKIKIRJAUS",
+                 "time_in_status"=>1805},
+
+                {"agent_id"=>1100039,
+                 "name"=>"kolmas test",
+                 "team"=>"Opiskelijaneuvonta",
+                 "status"=>"Sisäänkirjaus",
+                 "time_in_status"=>13616}]
+
     BackendService.any_instance.stub(:get_agent_online_state).and_return(expected)
-    agents = AgentsController.new
-    expect(agents.index[0][:name]).to eq("joku vaan")
-    expect(agents.index[1][:name]).to eq("testaus ukko")
-    expect(agents.index[2][:name]).to eq("kolmas test")
+    
+    get :index, format: :json
+    agents = JSON.parse(response.body)
+
+    # json tiedostossa on id attribuutti?? entiiämiks no tää poistaa sen
+    agents.each do |agent|
+      agent.delete("id")
+    end
+
+    expect(agents).to eq(expected_ilman_kaksoispisteitä_ja_name_eikä_full_name)
   end
 end
 
