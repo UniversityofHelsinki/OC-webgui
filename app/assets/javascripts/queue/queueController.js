3da206e17d9690a224f34741d1156d8aff82ab91
niklas <niklas@niklas.niklas>
2016-06-23 15:27:30 +0300

    initial commit for d3js graph in queue page

--- app/assets/javascripts/queue/queueController.js
+++ app/assets/javascripts/queue/queueController.js
@@ -1,4 +1,4 @@
-angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebGui.shared.time'])
+angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebGui.shared.time', 'nvd3'])
   .config(function ($stateProvider) {
     $stateProvider
       .state('queue', {
@@ -10,6 +10,37 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
   })
   .controller('QueueController', function ($interval, $scope, Queue) {
     var vm = this;
+
+    vm.options = {
+        chart: {
+            type: 'discreteBarChart',
+            // height: 400,
+            margin : {
+                top: 20,
+                right: 20,
+                bottom: 40,
+                left: 55
+            },
+            x: function(d) { return d.x; },
+            y: function(d) { return d.y; },
+            useInteractiveGuideline: true,
+            duration: 0,    
+            yDomain: [-1,1],
+            yAxis: {
+                tickFormat: function(d){
+                   return d3.format('.01f')(d);
+                }
+            }
+        }
+    };
+    
+    vm.data = [{ values: [
+      {x: 3, y: 0.3},
+      {x: 2, y: 0.6},
+      {x: 1, y: 0.8}
+      
+      ], key: 'Vastattujen puheluiden määrä' }];   
+
     var fetchDataInterval;
 
     vm.message = 'Jono';
@@ -24,6 +55,8 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
           return queuer.language !== 'Unknown';
         });
       });
+      // Also update date/time
+      vm.date = new Date();
     }
 
     fetchDataInterval = $interval(fetchData, 5000);
@@ -35,12 +68,12 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
 
     // mock data for testing css
     // vm.queue = [
-    //   { line: 135, time_in_queue: 360 },
-    //   { line: 137, time_in_queue: 123 },
-    //   { line: 125, time_in_queue: 123 },
-    //   { line: 137, time_in_queue: 123 },
-    //   { line: 133, time_in_queue: 123 },
-    //   { line: 137, time_in_queue: 123 },
-    //   { line: 121, time_in_queue: 123 }
+    //   { line: 135, language: 'Fin', time_in_queue: 360 },
+    //   { line: 137, language: 'Fin', time_in_queue: 123 },
+    //   { line: 125, language: 'Eng', time_in_queue: 123 },
+    //   { line: 137, language: 'Fin', time_in_queue: 123 },
+    //   { line: 133, language: 'Swe', time_in_queue: 53  },
+    //   { line: 131, language: 'Eng', time_in_queue: 123 },
+    //   { line: 121, language: 'Eng', time_in_queue: 214 }
     // ];
   });
