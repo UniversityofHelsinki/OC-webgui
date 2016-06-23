38bbaa9d7e544b8bb0146cde094996744c5702bc
Teemu Matvejeff <tamndrei567@gmail.com>
2016-06-23 12:50:38 +0300

    First implementation of method getting agent customer contacts.

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -18,10 +18,14 @@ class BackendService
 
  # Method for accessing customer contacts that specific agent
  # has had.
-  def get_agent_contacts
+  def get_agent_contacts (agent_id, start_date, end_date)
     message = { serviceGroupID: 4, serviceID: 137, teamID: "Helpdesk",
-    agentID: 2000049, startDate: '2016-06-14', endDate: '2016-06-15',
+    agentID: agent_id, startDate: start_date, endDate: end_date,
     contactTypes: 'PBX', useServiceTime: true }
+
+# agentID: 2000049
+# startDate: '2016-06-14'
+# endDate: '2016-06-15'
 
     reply = @client.call(:get_contacts, message: message)
     data = reply.body.dig(:get_contacts_response,
@@ -33,26 +37,29 @@ class BackendService
     data.map do |attrs|
       {
 	ticket_id: attrs[:string][0],
-	call_arrived_to_queue: 
-	queued_seconds: 
-	call_forwarded_to_agent: 
-	call_answered_by_agent: 
-	call_ended: 
-	call_handling_ended: 
-	call_length: 
-	call_handling_total: 
-	service_type: 
-	contact_direction: 
-	contact_type: 
-	contact_phone_num: 
-	contact_handler: 
-	contact_number: 
-	contact_state: 
-	contact_total_handling: 
-	sub_group:
+	call_arrived_to_queue: attrs[:string][1],
+	queued_seconds: attrs[:string][2],
+	call_forwarded_to_agent: attrs[:string][3],
+	call_answered_by_agent: attrs[:string][4],
+	call_ended: attrs[:string][5],
+	call_handling_ended: attrs[:string][6],
+	call_length: attrs[:string][7],
+	call_handling_total: attrs[:string][8],
+	service_type: attrs[:string][9],
+	contact_direction: attrs[:string][10],
+	contact_type: attrs[:string][11],
+	contact_phone_num: attrs[:string][12],
+	contact_handler: attrs[:string][13],
+	contact_number: attrs[:string][14],
+	contact_state: attrs[:string][15],
+	contact_total_handling: attrs[:string][16],
+	sub_group: attrs[:string][17]
       }
     end
+
+    data[0][:string] = 0
   end
+
 
   def get_agent_online_state
     reply = @client.call(:get_agent_online_state)
