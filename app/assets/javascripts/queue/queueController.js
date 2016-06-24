1d7555514fa2b7b1bd3cb3033ffe527508f308aa
niklas <niklas@niklas.niklas>
2016-06-24 23:22:43 +0300

    use lineplusbarchart instead of multichart

--- app/assets/javascripts/queue/queueController.js
+++ app/assets/javascripts/queue/queueController.js
@@ -13,7 +13,7 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
 
     vm.options = {
       chart: {
-        type: 'multiChart',
+        type: 'linePlusBarChart',
         height: 250,
         margin: {
           top: 30,
@@ -21,69 +21,62 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
           bottom: 60,
           left: 40
         },
-        color: d3.scale.category20().range(),
-        transitionDuration: 500,
+        // TODO tosi pöljätapa.........
+        x: function (d, i) { return i + 8; },
+        y: function (d, i) { return d[1]; },
+        bars: {
+          forceY: [0,111]
+        },
+        lines: {
+          forceY: [0,77]
+        },
         xAxis: {
           tickFormat: function(d) {
             return d3.format(',f')(d);
           },
-          ticks: 20,
-          // tickValues: [5, 7, 10, 15, 20]
+
+          axisLabel: "Kellonaika",
+          showMaxMin: true,
+          ticks: 10,
+          //tickValues: [5, 7, 10, 15, 20,24 ]
         },
-        yAxis1: {
-          tickFormat: function(d) {
-            return d3.format(',.1f')(d);
-          }
-        },
-        yAxis2: {
-          tickFormat: function(d) {
-            return d3.format(',.1f')(d);
-          }
-        },
-        yDomain1: [0,1.111],
-        yDomain2: [0,33]
       }
     };
+    // huonojuttu: menee epä synkkaan jos on eri määrä arvoja pylväällä ja 
+    // viiva/pistediagrammilla
+    vm.data = [{
+      'key': 'foo',
+      'bar': true,
+      'color': 'skyblue',
+      'values': [
+        [8, 22],
+        [9, 11],
+        [10, null],
+        [11, 26],
+        [12, 15],
+        [13, 65],
+        [14, 34],
+        [15, 25],
+        [16,  5],
+        [17, 14]
+      ]
+    }, {
+      'key': 'bar',
+      'color': 'steelblue',
+      'values': [
+        [8, 31],
+        [9, 22],
+        [10, 44],
+        [11, 22],
+        [12, 64],
+        [13, null],
+        [14, 13],
+        [15, 4],
+        [16, 75],
+        [17, 3]
+      ]
+    }];
 
-    vm.data = [{"key": "toine vaa",
-                "values": [
-                  {"x":0,"y":5.5},{"x":1,"y":12},
-                  {"x":2,"y":1.4},{"x":3,"y":2},
-                  {"x":4,"y":1.9},{"x":5,"y":11},
-                  {"x":6,"y":3.23},{"x":7,"y":7},
-                  {"x":8,"y":12.15},{"x":9,"y":4},
-                  {"x":10,"y":11.14},{"x":11,"y":3},
-                  {"x":12,"y":4.12},{"x":13,"y":13},
-                  {"x":14,"y":7.2},{"x":15,"y":1},
-                  {"x":16,"y":3.7},{"x":17,"y":20},
-                  {"x":18,"y":0.15},{"x":19,"y":0.2},
-                  {"x":20,"y":3.0},{"x":21,"y":13},
-                  {"x":22,"y":1.2},{"x":23,"y":0.12}],
-                "type": "line",
-                "yAxis": 2},
-
-                {"key": "joku", 
-                 "values": [
-                  {"x":0,"y":0.11974798073123002},{"x":1,"y":0.1895057559421199},
-                  {"x":2,"y":0.10314504511908851},{"x":3,"y":0.1666229648632272},
-                  {"x":4,"y":0.1887995123186846},{"x":5,"y":0.3035997203729298},
-                  {"x":6,"y":0.6473198551192336},{"x":7,"y":0.121955039397465},
-                  {"x":8,"y":0.4394235666363644},{"x":9,"y":0.3594654668035855},
-                  {"x":10,"y":0.9613337594176934},{"x":11,"y":0.5305646554680374},
-                  {"x":12,"y":0.28820972725225835},{"x":13,"y":0.23525337105928737},
-                  {"x":14,"y":0.3877272758522322},{"x":15,"y":0.2695719616482999},
-                  {"x":16,"y":0.0703105522401684},{"x":17,"y":0.510300398752299},
-                  {"x":18,"y":0.034558748435663},{"x":19,"y":0.119299364017256},
-                  {"x":20,"y":1.604822456569353},{"x":21,"y":0.704119424540316},
-                  {"x":22,"y":0.0235378351670104},{"x":23,"y":0.3092114658958694}], 
-                 "type": "bar",
-                 "yAxis": 1}];
-   
-    // arvon asettaminen
-    // vm.data[0]["values"][2]["y"] = 33;
-    // tai tälleen
-    // vm.data[0]["values"].push({"x": 27, "y": 11.11});
-    
     var fetchDataInterval;
     
     vm.message = 'Jono';
