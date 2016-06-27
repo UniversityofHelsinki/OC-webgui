89867d70c9d86bc8e2c834a9939bff4684a7f22e
banjohirvi <ollilari@gmail.com>
2016-06-27 14:30:37 +0300

    agent_statuses.json is now sorted by agent name

--- app/controllers/agent_statuses_controller.rb
+++ app/controllers/agent_statuses_controller.rb
@@ -3,7 +3,7 @@ class AgentStatusesController < ApplicationController
   # GET /agent_statuses
   # GET /agent_statuses.json
   def index
-    @agent_statuses = AgentStatus.where(open: true)
+    @agent_statuses = AgentStatus.where(open: true).sort { |a, b| a.name <=> b.name }
   end
   
 end
