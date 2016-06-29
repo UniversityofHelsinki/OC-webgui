aa76aeeace61d67a420b92fcfc8f72e26b763141
banjohirvi <ollilari@gmail.com>
2016-06-29 22:23:38 +0300

    Screw rubocop

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,6 @@
+# Provide API for retrieving Contacts data
 class ContactsController < ApplicationController
-
-	def today
-		@contacts = ContactsService.new.contacts_for_team('Helpdesk', "#{Time.zone.today} 2016-06-01 00:00:00", "#{Time.zone.today} 23:59:59")
-	end
-
+  def today
+    @contacts = ContactsService.new.contacts_for_team('Helpdesk', "#{Time.zone.today} 2016-06-01 00:00:00", "#{Time.zone.today} 23:59:59")
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,4 @@
 class TeamsController < ApplicationController
-
   def index
     @teams = Team.all
   end
