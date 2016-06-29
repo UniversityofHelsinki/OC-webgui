d09be29c90c0dfa14df21356e1cb57a492cecdcb
banjohirvi <ollilari@gmail.com>
2016-06-29 22:34:04 +0300

    Fixed many rubocop problems

--- spec/services/contacts_service_spec.rb
+++ spec/services/contacts_service_spec.rb
@@ -1,13 +1,12 @@
 RSpec.describe ContactsService, type: :service do
+  context 'when database contains some agent statuses that corrsepond to a contact' do
+    before(:example) do
+      time = Time.parse("#{Time.zone.today} 08:00:00")
 
-  context 'when database contains some agent statuses that corrsepond to a contact' do 
-    before (:example) do
-	  time = Time.parse("#{Time.zone.today} 08:00:00")
-
-	  # Another day	
-	  AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time - 1.day + (3.hours + 2.minutes), closed: time - 1.day + (4.hours + 10.minutes))
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time - 1.day + (3.hours + 40.minutes), closed: time - 1.day + (3.hours + 58.minutes))
-	  AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Chat', open: false, created_at: time - 1.day + (3.hours + 42.minutes), closed: time - 1.day + (3.hours + 48.minutes))
+      # Another day
+      AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time - 1.day + (3.hours + 2.minutes), closed: time - 1.day + (4.hours + 10.minutes))
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time - 1.day + (3.hours + 40.minutes), closed: time - 1.day + (3.hours + 58.minutes))
+      AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Chat', open: false, created_at: time - 1.day + (3.hours + 42.minutes), closed: time - 1.day + (3.hours + 48.minutes))
       # 8-9
       AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + 5.minutes, closed: time + 25.minutes)
       AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + 25.minutes, closed: time + 35.minutes)
@@ -18,38 +17,38 @@ RSpec.describe ContactsService, type: :service do
       AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (1.hour + 22.minutes), closed: time + (1.hour + 28.minutes))
       AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (1.hour + 30.minutes), closed: time + (1.hour + 42.minutes))
       AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (1.hour + 44.minutes), closed: time + (2.hours + 3.minutes))
-	  # 10-11
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (2.hours + 2.minutes), closed: time + (2.hours + 22.minutes))
-	  AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (2.hours + 10.minutes), closed: time + (2.hours + 58.minutes))
-	  AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Jälkikirjaus', open: false, created_at: time + (2.hours + 58.minutes + 25.seconds), closed: time + (3.hours + 5.minutes))
-	  # 11-12
-	  AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (3.hours + 2.minutes), closed: time + (4.hours + 10.minutes))
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (3.hours + 40.minutes), closed: time + (3.hours + 58.minutes))		    
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Jälkikirjaus', open: false, created_at: time + (3.hours + 58.minutes + 1.second), closed: time + (4.hours + 5.minutes))
-	  AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (3.hours + 42.minutes), closed: time + (3.hours + 48.minutes))
-	  # 12-13
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 15.minutes), closed: time + (4.hours + 35.minutes))
-	  AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 33.minutes), closed: time + (4.hours + 47.minutes))
-	  AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 50.minutes), closed: time + (5.hours + 2.minutes))
-	  # 13-14
-	  AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 4.minutes), closed: time + (5.hours + 22.minutes))
-	  AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 4.minutes), closed: time + (5.hours + 15.minutes))
-	  AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 27.minutes), closed: time + (5.hours + 55.minutes))
-	  AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (5.hours + 35.minutes), closed: time + (5.hours + 44.minutes))
-	  # 14-15
-	  AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (6.hours + 33.minutes), closed: time + (6.hours + 42.minutes))
-	  # 15-16
-	  AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (7.hours + 10.minutes), closed: time + (1.hour + 35.minutes))
-	  AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (7.hours + 34.minutes), closed: time + (1.hour + 52.minutes))
+      # 10-11
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (2.hours + 2.minutes), closed: time + (2.hours + 22.minutes))
+      AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (2.hours + 10.minutes), closed: time + (2.hours + 58.minutes))
+      AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Jälkikirjaus', open: false, created_at: time + (2.hours + 58.minutes + 25.seconds), closed: time + (3.hours + 5.minutes))
+      # 11-12
+      AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (3.hours + 2.minutes), closed: time + (4.hours + 10.minutes))
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (3.hours + 40.minutes), closed: time + (3.hours + 58.minutes))
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Jälkikirjaus', open: false, created_at: time + (3.hours + 58.minutes + 1.second), closed: time + (4.hours + 5.minutes))
+      AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (3.hours + 42.minutes), closed: time + (3.hours + 48.minutes))
+      # 12-13
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 15.minutes), closed: time + (4.hours + 35.minutes))
+      AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 33.minutes), closed: time + (4.hours + 47.minutes))
+      AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (4.hours + 50.minutes), closed: time + (5.hours + 2.minutes))
+      # 13-14
+      AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 4.minutes), closed: time + (5.hours + 22.minutes))
+      AgentStatus.create(agent_id: 225, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 4.minutes), closed: time + (5.hours + 15.minutes))
+      AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (5.hours + 27.minutes), closed: time + (5.hours + 55.minutes))
+      AgentStatus.create(agent_id: 123, team: 'Helpdesk', status: 'Chat', open: false, created_at: time + (5.hours + 35.minutes), closed: time + (5.hours + 44.minutes))
+      # 14-15
+      AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (6.hours + 33.minutes), closed: time + (6.hours + 42.minutes))
+      # 15-16
+      AgentStatus.create(agent_id: 124, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (7.hours + 10.minutes), closed: time + (1.hour + 35.minutes))
+      AgentStatus.create(agent_id: 262, team: 'Helpdesk', status: 'Puhelu', open: false, created_at: time + (7.hours + 34.minutes), closed: time + (1.hour + 52.minutes))
     end
 
     it 'creates Contact objects of those objects and returns them correctly' do
       contacts = ContactsService.new.contacts_for_team('Helpdesk', "#{Time.zone.today} 00:00:00", "#{Time.zone.today} 23:59:59")
       expect(contacts.length).to eq(22)
-      contacts.each { |c| expect(c.is_a? Contact).to be(true) }
+      contacts.each { |c| expect(c.is_a?(Contact)).to be(true) }
     end
 
-    it 'sets the call start and end times according to status creatiion and closing times' do 
+    it 'sets the call start and end times according to status creatiion and closing times' do
       contacts = ContactsService.new.contacts_for_team('Helpdesk', "#{Time.zone.today} 08:00:00", "#{Time.zone.today} 09:00:00")
       expect(contacts[0].answered).to eq("#{Time.zone.today} 05:05:00")
       expect(contacts[0].call_ended).to eq("#{Time.zone.today} 05:25:00")
