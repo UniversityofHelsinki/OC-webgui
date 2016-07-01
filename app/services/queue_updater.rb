b8ba7a4bdb352a0c1d9f09b3e06fffc60f9e07ce
banjohirvi <ollilari@gmail.com>
2016-07-01 15:10:27 +0300

    QueueUpdater initial version ready

--- app/services/queue_updater.rb
+++ app/services/queue_updater.rb
@@ -1,11 +1,11 @@
 class QueueUpdater
-
-  def initialize(timestamp)
-  	@timestamp = timestamp
+  # Current_time parameter should ALWAYS be current time (Time.zone.now), except for tests
+  def initialize(current_time)
+    @timestamp = current_time
   end
 
   def update_queue(new_queue)
-  	previous_queue = QueueItem.where(open: true)
+    previous_queue = QueueItem.where(open: true)
     previous_queue = [] unless previous_queue
     new_queue = [] unless new_queue
 
@@ -21,35 +21,34 @@ class QueueUpdater
   private
 
     def assign_created_at(items)
-    	items.each { |item| item.created_at = Time.at(@timestamp.to_i - item.time_in_queue) }
+      items.each { |item| item.created_at = Time.at(@timestamp.to_i - item.time_in_queue) }
     end
 
     def update_items_left_queue
-    	closed = []
-    	@previous_queue.each do |item|
-    		matches = @new_queue.select { |match| match.created_at <= item.created_at + 1.second &&
-    		                                     match.created_at >= item.created_at - 1.second && 
-    		                                     match.line == item.line &&
-    		                                     match.label == item.label }
-    		unless matches.any?
-    			item.open = false
-    			item.closed = Time.at(Time.zone.now.to_i)
-    			item.save
-    		end
-    	end
-    	
+      @previous_queue.each do |item|
+        matches = @new_queue.select { |match| match.created_at <= item.created_at + 1.second &&
+                                              match.created_at >= item.created_at - 1.second && 
+                                              match.line == item.line &&
+                                              match.label == item.label }
+        unless matches.any?
+          item.open = false
+          item.closed = Time.at(Time.zone.now.to_i)
+          item.save
+        end
+      end
+      
     end
 
     def update_new_queue_items
 
-    	@new_queue.each do |item|
-    		matches = @previous_queue.select { |match| match.created_at <= item.created_at + 1.second &&
-    		                                     match.created_at >= item.created_at - 1.second && 
-    		                                     match.line == item.line &&
-    		                                     match.label == item.label }
-    		
-    		QueueItem.create(created_at: item.created_at, line: item.line, label: item.label, open: true) unless matches.any?
-    	end
+      @new_queue.each do |item|
+        matches = @previous_queue.select { |match| match.created_at <= item.created_at + 1.second &&
+                                                   match.created_at >= item.created_at - 1.second && 
+                                                   match.line == item.line &&
+                                                   match.label == item.label }
+        
+        QueueItem.create(created_at: item.created_at, line: item.line, label: item.label, open: true) unless matches.any?
+      end
 
     end
 
--- spec/services/queue_updater_spec.rb
+++ spec/services/queue_updater_spec.rb
@@ -2,68 +2,68 @@ RSpec.describe QueueUpdater, type: :service do
 
 	def queue_a_1
 		[
-			QueueItem.new(line: "131", label: "Neuvonta Eng", time_in_queue: "10"),
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "22")
+			QueueItem.new(line: '131', label: 'Neuvonta Eng', time_in_queue: '10'),
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '22')
 		]
 	end
 
 	def queue_a_2
 		[
-			QueueItem.new(line: "131", label: "Neuvonta Eng", time_in_queue: "14"),
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "26")
+			QueueItem.new(line: '131', label: 'Neuvonta Eng', time_in_queue: '14'),
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '26')
 		]	
 	end
 
 	def queue_a_3
 		[
-			QueueItem.new(line: "131", label: "Neuvonta Eng", time_in_queue: "18")
+			QueueItem.new(line: '131', label: 'Neuvonta Eng', time_in_queue: '18')
 		]
 	end
 
 	def queue_b_1
 		[
-			QueueItem.new(line: "131", label: "Neuvonta Eng", time_in_queue: "12")
+			QueueItem.new(line: '131', label: 'Neuvonta Eng', time_in_queue: '12')
 		]
 	end
 
 	def queue_b_2
 		[
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "12")
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '12')
 		]
 	end
 
 	def queue_c_1
 		[
-			QueueItem.new(line: "131", label: "Nevuonta Eng", time_in_queue: "3")
+			QueueItem.new(line: '131', label: 'Nevuonta Eng', time_in_queue: '3')
 		]
 	end
 
 	def queue_d_1
 		[
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "5"),
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "5")
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '5'),
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '5')
 		]
 	end
 
 	def queue_d_2
 		[
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "5"),
-			QueueItem.new(line: "161", label: "Hakijapalvelut Fin", time_in_queue: "12")
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '5'),
+			QueueItem.new(line: '161', label: 'Hakijapalvelut Fin', time_in_queue: '12')
 		]
 	end
 
-	it "works with nil inputs" do 
+	it 'works with nil inputs' do 
 		QueueUpdater.new(Time.zone.now).update_queue([])
 		QueueUpdater.new(Time.zone.now).update_queue(nil)
     end
 
-	context "when two items enter an empty queue" do
+	context 'when two items enter an empty queue' do
 
 		before (:example) do 
 			QueueUpdater.new(Time.zone.now).update_queue(queue_a_1)
 		end
 
-		it "creates new open QueueItem objects for each new queue item and stores them in the DB" do
+		it 'creates new open QueueItem objects for each new queue item and stores them in the DB' do
 			expect(QueueItem.all.length).to eq(2)
 			expect(QueueItem.where(open: true).length).to eq(2)
 		end
@@ -75,7 +75,24 @@ RSpec.describe QueueUpdater, type: :service do
 
 	end
 
-	context "when the two same items appear in the queue on subsequent searches" do
+	context 'when two items are in the queue and then disappear' do
+		before(:example) do 
+			QueueUpdater.new(Time.zone.now).update_queue(queue_a_1)
+			QueueUpdater.new(Time.zone.now + 15.seconds).update_queue([])
+		end
+
+		it 'creates and then closes a new QueueItem object for each of them' do 
+			expect(QueueItem.all.length).to eq(2)
+			expect(QueueItem.where(open: false).length).to eq(2)
+			expect(QueueItem.first.closed).to eq(Time.at(Time.zone.now.to_i))
+			expect(QueueItem.first.created_at).to eq(Time.at(Time.zone.now.to_i - 10.seconds))
+			expect(QueueItem.second.closed).to eq(Time.at(Time.zone.now.to_i))
+			expect(QueueItem.second.created_at).to eq(Time.at(Time.zone.now.to_i - 22.seconds))
+		end
+
+	end
+
+	context 'when two same items appear in the queue on subsequent searches with correctly increased time_in_status' do
 
 		before (:example) do			
 			QueueUpdater.new(Time.zone.now).update_queue(queue_a_1)
@@ -92,7 +109,7 @@ RSpec.describe QueueUpdater, type: :service do
 
 	end
 
-	context "when two items are in the queue, and one of them disappears" do
+	context 'when two items are in the queue, and one of them disappears' do
 
 		before (:example) do 
 			QueueUpdater.new(Time.zone.now).update_queue(queue_a_2)
@@ -103,52 +120,30 @@ RSpec.describe QueueUpdater, type: :service do
 			expect(QueueItem.all.length).to eq(2)
 		end
 
-		it "closes the item which disappeared" do
+		it 'closes the item which disappeared' do
 			expect(QueueItem.second.open).to be(false)
 		end
 
-		it "sets the time the item closed correctly" do
+		it 'sets the time the item closed correctly' do
 			expect(QueueItem.second.closed).to eq(Time.at(Time.zone.now.to_i))
 		end
 	end
 
-	context "When an item in the queue disappears, and another with similar time turns up" do
+	context 'When an item in the queue disappears, and another with similar time turns up' do
 
 		before (:example) do 
 			QueueUpdater.new(Time.zone.now).update_queue(queue_b_1)
 			QueueUpdater.new(Time.zone.now + 12.seconds).update_queue(queue_b_1)
 		end
 
-		it "closes the first item" do
+		it 'closes the first item' do
 			expect(QueueItem.first.open).to be(false)
 		end
 
-		it "opens a new QueueStatus object for the new item" do 
+		it 'opens a new QueueStatus object for the new item' do 
 			expect(QueueItem.all.length).to be(2)
 			expect(QueueItem.second.open).to be(true)
 		end
 	end
-
-	context "When the same result is returned several times over a short period of time" do
-
-		before (:example) do
-			QueueUpdater.new(Time.zone.now).update_queue(queue_c_1)
-			QueueUpdater.new(Time.zone.now + 1.second).update_queue(queue_c_1)
-			QueueUpdater.new(Time.zone.now + 2.seconds).update_queue(queue_c_1)
-		end
-
-		it "doesn't create a new QueueItem for the object" do
-			
-		end
-
-	end
-
-
-
-
-
-  
-
-
 
 end
