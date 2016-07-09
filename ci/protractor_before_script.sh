7736b3049aa02cebdbdfcf484e76932f01e5ead2
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-09 13:30:37 +0300

    More more more more test

--- ci/protractor_before_script.sh
+++ ci/protractor_before_script.sh
@@ -1,6 +1,4 @@
 nvm use 4.0
 psql -c 'create database ocwebgui_test;' -U postgres
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x32
-sleep 3
 bundle exec rails server -p 3000 & 
 sleep 5
