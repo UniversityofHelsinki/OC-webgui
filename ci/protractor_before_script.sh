6555d3617210a6ce9815cbc5e1e391610352f573
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-09 13:23:34 +0300

    More more more test

--- ci/protractor_before_script.sh
+++ ci/protractor_before_script.sh
@@ -1,7 +1,6 @@
 nvm use 4.0
 psql -c 'create database ocwebgui_test;' -U postgres
-export DISPLAY=:99.0
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x32
-sh -e /etc/init.d/xvfb start
+sleep 3
 bundle exec rails server -p 3000 & 
 sleep 5
--- ci/protractor_script.sh
+++ ci/protractor_script.sh
@@ -1,1 +1,2 @@
+export DISPLAY=:99.0
 node_modules/.bin/protractor e2e-tests/protractor.conf.js --browser=firefox
