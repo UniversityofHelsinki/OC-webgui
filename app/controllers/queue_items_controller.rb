f5d9ff98f29809082c93d8bc088225fc6aa7c2d9
niklas <niklas@niklas.niklas>
2016-07-13 15:20:38 +0300

    rename queues to queue_items

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -17,7 +17,7 @@ class QueueItemsController < ApplicationController
   def stats
     render json: {
       average_waiting_time: @queue_service.average_queueing_duration(@team_name, @start_time, @end_time),
-      queues_by_hour: @queue_service.queues_by_hour(@team_name, @start_time, @end_time)
+      queue_items_by_hour: @queue_service.queue_items_by_hour(@team_name, @start_time, @end_time)
     }
   end
 end
--- app/services/queue_service.rb
+++ app/services/queue_service.rb
@@ -3,27 +3,27 @@ class QueueService
   end
 
   def average_queueing_duration(team_name, start_time, end_time)
-    average_duration(contact_queues(team_name, start_time, end_time))
+    average_duration(contact_queue_items(team_name, start_time, end_time))
   end
 
-  def average_duration(queues)
-    queues.select('ROUND(AVG(EXTRACT(EPOCH FROM closed - created_at))) AS average_duration')[0]['average_duration'] || 0
+  def average_duration(queue_items)
+    queue_items.select('ROUND(AVG(EXTRACT(EPOCH FROM closed - created_at))) AS average_duration')[0]['average_duration'] || 0
   end
 
-  def queues(team_name, start_time, end_time)
+  def queue_items(team_name, start_time, end_time)
     QueueItem.where(open: false, label: team_name, created_at: start_time..end_time, closed: start_time..end_time)
   end
 
-  def queues_by_hour(team_name, start_time, end_time)
+  def queue_items_by_hour(team_name, start_time, end_time)
     gmt_offset = Time.now.getlocal.gmt_offset
     select = "EXTRACT(HOUR FROM created_at + '#{gmt_offset} seconds') AS hour, COUNT(*) AS count"
-    data = contact_queues(team_name, start_time, end_time).select(select).group('hour')
+    data = contact_queue_items(team_name, start_time, end_time).select(select).group('hour')
     result = Array.new(24, 0)
     data.each { |d| result[(d['hour'])] = d['count'] }
     result
   end
 
-  def contact_queues(team_name, start_time, end_time)
-    queues(team_name, start_time, end_time)
+  def contact_queue_items(team_name, start_time, end_time)
+    queue_items(team_name, start_time, end_time)
   end
 end
