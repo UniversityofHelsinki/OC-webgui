3de734e3320d47a30355906190d8f4d50461c8f9
Teemu Matvejeff <tamndrei567@gmail.com>
2016-07-14 11:37:50 +0300

    Boilerplate for personal agent view.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
 angular.module('ocWebGui', ['templates', 'ocWebGui.home', 'ocWebGui.screen',
-  'ocWebGui.queue', 'ocWebGui.filterpanel', 'ocWebGui.stats', 'ocWebGui.login'])
+  'ocWebGui.queue', 'ocWebGui.filterpanel', 'ocWebGui.stats', 'ocWebGui.login', 
+  'ocWebGui.personal'])
   .run(function ($rootScope, $state, User) {
     $rootScope.returnToState = 'home';
     $rootScope.returnToParams = {};
