ea6880ce48eaa5b9f4d35fb4a3c88af8d6e92b1a
Teemu Matvejeff <tamndrei567@gmail.com>
2016-07-20 15:01:42 +0300

    Fix Rspec time mocking

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -4,22 +4,23 @@ require Rails.root.to_s + '/app/controllers/application_controller.rb'
 require Rails.root.to_s + '/app/services/backend_service.rb'
 
 RSpec.describe QueueItemsController, type: :controller do
-  include Now
   render_views
+
+  time = Time.parse('2016-07-11T10:30:46.000Z')
 
   it 'queue json should work' do
     item1 = { "line"=>136,
               "label"=>"abc",
-              "created_at"=>anything
+              "created_at"=>'2016-07-11T10:30:26.000Z'
             }
 
     item2 = { "line"=>133,
               "label"=>"qwe",
-              "created_at"=>anything
+              "created_at"=>'2016-07-11T10:30:35.000Z'
             }
 
-    QueueItem.create(line: 136, label: "abc", created_at: now - 20.seconds, open: true)
-    QueueItem.create(line: 133, label: "qwe", created_at: now - 11.seconds, open: true)
+    QueueItem.create(line: 136, label: "abc", created_at: time - 20.seconds, open: true)
+    QueueItem.create(line: 133, label: "qwe", created_at: time - 11.seconds, open: true)
     get :index, format: :json
     queueitems = JSON.parse(response.body)
 
