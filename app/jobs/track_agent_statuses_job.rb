6945be8e87f4b690c8e1d081ac2c6ba246f6dc63
niklas <niklas@niklas.niklas>
2016-07-21 18:44:02 +0300

    refactor lunch thing to its own method in track_agent_statuses_job

--- app/jobs/track_agent_statuses_job.rb
+++ app/jobs/track_agent_statuses_job.rb
@@ -4,20 +4,21 @@ class TrackAgentStatusesJob
 
   def perform
     current = BackendService.new.get_agent_online_state.map do |data|
-      if data[:status] == 'Ruokatunti'
-        agent_id = data[:agent_id].to_i
-        luncheds = Rails.cache.fetch('lunched', force: true)
-        luncheds = [] if luncheds.nil?
-        luncheds.push(agent_id)
-        luncheds = luncheds.uniq
-        Rails.cache.fetch('lunched', force: true) { luncheds }
-      end
+      agent_has_eaten(data) if data[:status] == 'Ruokatunti'
       AgentStatus.new(agent: Agent.find_or_create(data[:agent_id], data[:name], data[:team]),
                       status: data[:status],
                       time_in_status: data[:time_in_status])
     end
     log = JobLog.new('TrackAgentStatusesJob')
     AgentStatusUpdater.new(now, log.last_success).update_statuses(current)
+  end
+
+  def agent_has_eaten(data)
+    agent_id = data[:agent_id].to_i
+    luncheds = Rails.cache.fetch('lunched', force: true)
+    return if luncheds.nil?
+    luncheds.push(agent_id) unless luncheds.contains? agent_id
+    Rails.cache.fetch('lunched', force: true) { luncheds }
   end
 
   def max_run_time
