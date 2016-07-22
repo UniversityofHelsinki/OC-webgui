084b27d3739bf83e8635323141a78060324af9d6
niklas <niklas@niklas.niklas>
2016-07-22 04:45:23 +0300

    modify applicationrb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module OCWebgui
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Helsinki'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -23,7 +24,5 @@ module OCWebgui
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
-
-    config.time_zone = 'Helsinki'
   end
 end
