f2e0cc2e441def3978ca3a809fe3a89a5141d2b3
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-22 19:43:21 +0300

    Show navbar when mouse is moved anywhere

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,7 @@
 angular.module('ocWebGui', ['templates', 'ocWebGui.home', 'ocWebGui.screen',
   'ocWebGui.queue', 'ocWebGui.filterpanel', 'ocWebGui.stats', 'ocWebGui.login',
   'ocWebGui.navbar', 'ocWebGui.personal'])
-  .run(function ($rootScope, $state, User) {
+  .run(function ($rootScope, $state, User, $timeout) {
     $rootScope.returnToState = 'home';
     $rootScope.returnToParams = {};
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
@@ -11,5 +11,20 @@ angular.module('ocWebGui', ['templates', 'ocWebGui.home', 'ocWebGui.screen',
         event.preventDefault();
         $state.go('login');
       }
+    });
+
+    var $body = $(document.body);
+
+    function createHideMouseTimeout() {
+      return $timeout(function () {
+        $body.addClass('hide-mouse');
+      }, 2000);
+    }
+
+    var mouseHideTimeout = createHideMouseTimeout();
+    $body.on('mousemove', function () {
+      $body.removeClass('hide-mouse');
+      $timeout.cancel(mouseHideTimeout);
+      mouseHideTimeout = createHideMouseTimeout();
     });
   });
