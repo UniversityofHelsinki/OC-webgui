3e3b46874f6cc95c052d2181c5dd9ee1cce03d65
banjohirvi <ollilari@gmail.com>
2016-07-23 20:53:13 +0300

    Fixed ambigious column error when calculating queueitems' average duration

--- app/services/queue_service.rb
+++ app/services/queue_service.rb
@@ -8,16 +8,16 @@ class QueueService
   end
 
   def average_duration(queue_items)
-    queue_items.select('ROUND(AVG(EXTRACT(EPOCH FROM closed - created_at))) AS average_duration')[0]['average_duration'] || 0
+    queue_items.select('ROUND(AVG(EXTRACT(EPOCH FROM closed - queue_items.created_at))) AS average_duration')[0]['average_duration'] || 0
   end
 
   def queue_items(team_name, start_time, end_time)
-    QueueItem.joins(service: :team).where(open: false, teams: { name: team_name }, created_at_: start_time..end_time)
+    QueueItem.joins(service: :team).where(open: false, teams: { name: team_name }, created_at: start_time..end_time)
   end
 
   def queue_items_by_hour(team_name, start_time, end_time)
     gmt_offset = Time.now.getlocal.gmt_offset
-    select = "EXTRACT(HOUR FROM created_at + '#{gmt_offset} seconds') AS hour, COUNT(*) AS count"
+    select = "EXTRACT(HOUR FROM queue_items.created_at + '#{gmt_offset} seconds') AS hour, COUNT(*) AS count"
     data = contact_queue_items(team_name, start_time, end_time).select(select).group('hour')
     result = Array.new(24, 0)
     data.each { |d| result[(d['hour'])] = d['count'] }
