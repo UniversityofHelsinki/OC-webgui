8eb62a174fd35cbf5430d24dad4d2b178dccbd1c
banjohirvi <ollilari@gmail.com>
2016-07-26 12:20:35 +0300

    Now fetches daily contacts every 30 seconds

--- job_schedule.rb
+++ job_schedule.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift(app_path) unless $LOAD_PATH.include?(app_path)
 require 'backburner'
 require 'app/modules/now.rb'
 require 'config/initializers/backburner.rb'
+require 'active_support'
+require 'active_support/core_ext/time'
 Dir[File.dirname(__FILE__) + '/app/jobs/*.rb'].each { |file| require file }
 
 def xsd_time(time)
@@ -15,10 +17,10 @@ module Clockwork
   every(5.seconds, 'track_agent_statuses.job') { Backburner.enqueue TrackAgentStatusesJob }
   every(1.second, 'track_queue_items.job') { Backburner.enqueue TrackQueueItemsJob }
   every(1.day, 'clear_rails_cache.job', at: '00:00', tz: 'UTC') { BackBurner.enqueue CacheDeleteJob, 'lunched' }
-  every(1.day, 'get_daily_helpdesk_contacts.job', at: '02:30') do
+  every(30.seconds, 'get_helpdesk_contacts.job') do
     Backburner.enqueue GetTeamContactsJob,
                        'Helpdesk',
-                       xsd_time(Time.now - 48 * 60 * 60),
-                       xsd_time(Time.now)
+                       xsd_time(Time.now.beginning_of_day),
+                       xsd_time(Time.now.end_of_day)
   end
 end
