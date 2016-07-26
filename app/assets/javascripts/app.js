50248d8dfa50a08d7bb14f7b900d7f213f8c2fce
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-26 20:34:01 +0300

    Create overlay and always visible navbar styles

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,18 +2,9 @@ angular.module('ocWebGui', ['templates', 'ocWebGui.home', 'ocWebGui.screen',
   'ocWebGui.queue', 'ocWebGui.filterpanel', 'ocWebGui.stats', 'ocWebGui.login',
   'ocWebGui.navbar', 'ocWebGui.personal'])
   .run(function ($rootScope, $state, User, $interval) {
-    $rootScope.returnToState = 'home';
-    $rootScope.returnToParams = {};
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-      if (toState.name === 'stats' && !User.isAuthenticated()) {
-        $rootScope.returnToState = toState;
-        $rootScope.returnToParams = toParams;
-        event.preventDefault();
-        $state.go('login');
-      }
-    });
-
     var $body = $(document.body);
+    var $navbar = $('.navbar');
+    var mouseHideTimeout;
 
     function createHideMouseTimeout() {
       return $interval(function () {
@@ -21,10 +12,40 @@ angular.module('ocWebGui', ['templates', 'ocWebGui.home', 'ocWebGui.screen',
       }, 2000, 1);
     }
 
-    var mouseHideTimeout = createHideMouseTimeout();
-    $body.on('mousemove', function () {
-      $body.removeClass('hide-mouse');
+    function activateNavbarOverlay() {
       $interval.cancel(mouseHideTimeout);
       mouseHideTimeout = createHideMouseTimeout();
+
+      $navbar.addClass('overlay');
+      $body.on('mousemove', function () {
+        $body.removeClass('hide-mouse');
+        $interval.cancel(mouseHideTimeout);
+        mouseHideTimeout = createHideMouseTimeout();
+      });
+    }
+
+    function disableNavbarOverlay() {
+      $interval.cancel(mouseHideTimeout);
+
+      $navbar.removeClass('overlay');
+      $body.removeClass('hide-mouse').off('mousemove');
+    }
+
+    $rootScope.returnToState = 'home';
+    $rootScope.returnToParams = {};
+
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+      if (toState.navbarOverlay === true) {
+        activateNavbarOverlay();
+      } else {
+        disableNavbarOverlay();
+      }
+
+      if (toState.name === 'stats' && !User.isAuthenticated()) {
+        $rootScope.returnToState = toState;
+        $rootScope.returnToParams = toParams;
+        event.preventDefault();
+        $state.go('login');
+      }
     });
   });
