5412d4c58686df7c0a4138edff9c7661cb45c3ea
niklas <niklas@niklas.niklas>
2016-07-28 19:22:33 +0300

    camelCaseify queue controller

--- app/assets/javascripts/queue/queueController.js
+++ app/assets/javascripts/queue/queueController.js
@@ -71,11 +71,11 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
     function fetchContactStats() {
       $http.get('contacts/stats.json').then(function (response) {
         var data = response.data;
-        var calls_values = data.calls_by_hour
+        var callsValues = data.calls_by_hour
           .map(function (calls, hour) { return { hour: hour, calls: calls }; })
           .filter(function (item) { return item.hour >= 8 && item.hour <= 18; });
 
-        var queue_values = data.average_queue_duration_by_hour
+        var queueValues = data.average_queue_duration_by_hour
           .map(function (calls, hour) { return { hour: hour, calls: calls }; })
           .filter(function (item) { return item.hour >= 8 && item.hour <= 18; });
 
@@ -84,10 +84,10 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
         }
 
         angular.extend(vm.stats, vm.stats, data);
-        vm.data[0].values = calls_values;
-        vm.data[1].values = queue_values;
-        vm.options.chart.bars.yDomain[1] = get_nearest_ten(0);
-        vm.options.chart.lines.yDomain[1] = get_nearest_ten(1);
+        vm.data[0].values = callsValues;
+        vm.data[1].values = queueValues;
+        vm.options.chart.bars.yDomain[1] = getNearestTen(0);
+        vm.options.chart.lines.yDomain[1] = getNearestTen(1);
       });
     }
 
@@ -101,12 +101,12 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
       });
     }
 
-    function get_nearest_ten(i) {
-      var max_val = d3.max(vm.data[i].values, function (x) { return x.calls; });
-      if (max_val == null) {
+    function getNearestTen(i) {
+      var maxVal = d3.max(vm.data[i].values, function (x) { return x.calls; });
+      if (maxVal == null) {
         return 0;
       }
-      return Math.ceil(max_val / 10) * 10;
+      return Math.ceil(maxVal / 10) * 10;
     }
 
     vm.message = 'Jono';
