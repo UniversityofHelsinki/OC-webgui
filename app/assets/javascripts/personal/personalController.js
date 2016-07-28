b1da3ba0eaabf3de4af8854cc993576e5a5ee8a5
jKostet <juho.kostet@helsinki.fi>
2016-07-28 13:39:07 +0300

    Personal view myStatus

--- app/assets/javascripts/personal/personalController.js
+++ app/assets/javascripts/personal/personalController.js
@@ -1,4 +1,4 @@
-angular.module('ocWebGui.personal', ['ui.router', 'ocWebGui.screen.service', 'ocWebGui.queue.service', 'ocWebGui.personal.service'])
+angular.module('ocWebGui.personal', ['ui.router', 'ocWebGui.screen.service', 'ocWebGui.queue.service', 'ocWebGui.personal.service', 'ocWebGui.login'])
   .config(function ($stateProvider) {
     $stateProvider
       .state('personal', {
@@ -8,19 +8,27 @@ angular.module('ocWebGui.personal', ['ui.router', 'ocWebGui.screen.service', 'oc
         controllerAs: 'personal'
       });
   })
-  .controller('PersonalController', function (Agents, Queue, Personal, $interval, $scope) {
+  .controller('PersonalController', function (Agents, Queue, User, Personal, $interval, $scope) {
     var vm = this;
 
     function fetchData() {
       Agents.query(function (agents) {
         vm.agents = agents;
+        vm.myStatus = agents.filter(function (agent) {
+          return User.getUserData().agent_id == agent.id;
+        })[0].status;
       });
       Queue.query(function (queue) {
         vm.queue = queue;
       });
-      Personal.getPersonalData(function (data) {
-        vm.personalData = data;
-      });
+      // Personal.getPersonalData().then(function (data) {
+      //     console.log(data);
+      //   vm.personalData = data;
+      //   vm.personalData.myStatus = agents.filter(function () {
+      //       return agent_id == data.agent_id;
+      //   })[0].status;
+      //     console.log(vm.personalData);
+      // });
     }
 
     var fetchDataInterval = $interval(fetchData, 5000);
--- app/assets/javascripts/personal/personalService.js
+++ app/assets/javascripts/personal/personalService.js
@@ -3,45 +3,7 @@ angular.module('ocWebGui.personal.service', ['ngResource', 'ocWebGui.filterpanel
     var userData = User.getUserData();
     return {
       getPersonalData: function () {
-        $http.post('personal.json', { agent_id: userData.agent_id })
-          .then(function (response) {
-            return response;
-          });
+        return $http.post('personal.json', { agent_id: userData.agent_id });
       }
     };
   });
-
-
-/*
-      });
-  })
-  .factory('User', function ($http) {
-    var isAuthenticated = false;
-    var userData = { username: null };
-    return {
-      login: function (username, password, onSuccess, onError) {
-        $http.post('login', { username: username, password: password })
-          .then(function (response) {
-            isAuthenticated = true;
-            userData = response.data;
-            onSuccess();
-          }, function (response) {
-            isAuthenticated = false;
-            onError(response.data.error);
-          });
-      },
-      isAuthenticated: function () {
-        return isAuthenticated;
-      },
-      getUsername: function () {
-        return userData.username;
-      },
-      logout: function () {
-        $http.delete('logout')
-          .then(function () {
-            isAuthenticated = false;
-            userData = { username: null };
-          });
-      }
-    };
-*/
