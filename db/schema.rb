46bceaeb752a9708948ebc8508dd7505fedfac1c
Tuomas Siipola <siiptuo@kapsi.fi>
2016-07-29 20:40:51 +0300

    Implement initial settings API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728103701) do
+ActiveRecord::Schema.define(version: 20160729095311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20160728103701) do
     t.datetime "updated_at",                      null: false
     t.integer  "agent_id"
     t.boolean  "is_admin",        default: false, null: false
+    t.json     "settings",        default: {},    null: false
   end
 
   add_foreign_key "agent_statuses", "agents"
