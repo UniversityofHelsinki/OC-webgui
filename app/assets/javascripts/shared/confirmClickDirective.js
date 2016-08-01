a101437609805a5762c6e1a60ceac4de33fad93c
banjohirvi <ollilari@gmail.com>
2016-08-01 14:03:24 +0300

    Some more code style fixes

--- app/assets/javascripts/shared/confirmClickDirective.js
+++ app/assets/javascripts/shared/confirmClickDirective.js
@@ -1,15 +1,15 @@
 angular.module('ocWebGui.shared.confirmClick', [])
-  .directive('ocConfirmClick', [
-        function(){
-            return {
-                link: function (scope, element, attr) {
-                    var msg = attr.ocConfirmClick || "Oletko varma?";
-                    var clickAction = attr.confirmedClick;
-                    element.bind('click',function (event) {
-                        if ( window.confirm(msg) ) {
-                            scope.$eval(clickAction)
-                        }
-                    });
-                }
-            };
-    }])
+.directive('ocConfirmClick', [
+  function () {
+    return {
+      link: function (scope, element, attr) {
+        var msg = attr.ocConfirmClick || 'Oletko varma?';
+        var clickAction = attr.confirmedClick;
+        element.bind('click', function (event) {
+          if (window.confirm(msg)) {
+            scope.$eval(clickAction);
+          }
+        });
+      }
+    };
+  }]);
