8b0cec8ad08de34cda9044a1c887469af5d07714
Teemu Matvejeff <tamndrei567@gmail.com>
2016-08-02 15:35:05 +0300

    First commit to color preview

---
+++ app/assets/javascripts/color_management/colorManagementController.js
@@ -0,0 +1,14 @@
+angular.module('ocWebGui.colormanagement', ['ui.router', 'ocWebGui.shared.time'])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('colormanagement', {
+        url: '/colors',
+        templateUrl: 'colormanagement/_colormanagement.html',
+        controller: 'colorManagementController',
+        controllerAs: 'colormanagement',
+        navbarOverlay: true
+      });
+  })
+  .controller('colorManagementController', function () {
+    
+  });
