e3b09508f12636d61faccc82b4b965d358686007
niklas <niklas@niklas.niklas>
2016-08-02 15:45:01 +0300

    Updated rspec test

--- spec/services/contacts_service_spec.rb
+++ spec/services/contacts_service_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe ContactsService, type: :service do
     end
 
     it "Returns correct queue durations by times" do
-      expect(@contacts_service.queue_durations_by_times).to eq([['2016-07-18 05:00:00.000000000 +0000', 60.0], ['2016-07-18 08:00:00.000000000 +0000', 240.0], ['2016-07-18 09:00:00.000000000 +0000', 120.0]])
+      expect(@contacts_service.queue_durations_by_times).to eq([['2016-07-18 08:00:00.000000000 +0000', 60.0], ['2016-07-18 11:00:00.000000000 +0000', 240.0], ['2016-07-18 12:00:00.000000000 +0000', 120.0]])
     end
   end
 end
