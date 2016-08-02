5aead1d4d429a62a57c57cdb21ae5226be9c268b
banjohirvi <ollilari@gmail.com>
2016-08-02 14:59:57 +0300

    Fetch stats every 30s

--- app/assets/javascripts/queue/queueController.js
+++ app/assets/javascripts/queue/queueController.js
@@ -109,7 +109,7 @@ angular.module('ocWebGui.queue', ['ocWebGui.queue.service', 'ui.router', 'ocWebG
       vm.date = new Date();
     }
     var fetchDataInterval = $interval(fetchData, 5 * 1000);
-    var fetchContactStatsInterval = $interval(fetchContactStats, 5 * 60 * 1000);
+    var fetchContactStatsInterval = $interval(fetchContactStats, 30 * 1000);
     $scope.$on('$destroy', function () {
       $interval.cancel(fetchDataInterval);
       $interval.cancel(fetchContactStatsInterval);
