dfa69ffb2ec5a80c06cc79b237fe8976502cacaa
Tuomas Siipola <siiptuo@kapsi.fi>
2016-08-04 18:01:50 +0300

    Refactor personal controller and template

--- app/assets/javascripts/personal/personalController.js
+++ app/assets/javascripts/personal/personalController.js
@@ -21,17 +21,13 @@ angular.module('ocWebGui.personal', ['ui.router', 'ocWebGui.screen.service', 'oc
     function fetchData() {
       $q.all({ agents: Agents.query(), userData: User.getUserData() }).then(function (values) {
         vm.agents = values.agents;
-        var myAgent = values.agents.find(function (agent) {
+        vm.currentAgent = vm.agents.find(function (agent) {
           return values.userData.agent_id === agent.id;
         });
-
-        if (myAgent === undefined) {
-          vm.myColor = 'grey';
-          vm.myStatus = 'Offline';
-        } else {
-          vm.myColor = myAgent.color;
-          vm.myStatus = myAgent.status;
-          values.agents.splice(values.agents.indexOf(myAgent), 1);
+        if (vm.currentAgent) {
+          vm.agents = vm.agents.filter(function (agent) {
+            return agent !== vm.currentAgent;
+          });
         }
       });
 
