2683e4f5ffe09f26be9687df4312428e82194904
niklas <niklas@niklas.niklas>
2016-08-04 21:17:36 +0300

    text inputs to number inputs

--- app/assets/javascripts/others/othersController.js
+++ app/assets/javascripts/others/othersController.js
@@ -29,6 +29,9 @@ angular.module('ocWebGui.others', ['ui.router', 'ocWebGui.login'])
       clearMessage();
     });
     vm.save = function () {
+      console.log("--------");
+ //     vm.others.working_day_start = parseInt(vm.others.working_day_start);
+      console.log(vm.others);
       Settings.setOthers(vm.others).then(function () {
         vm.message = 'Kivat valinnat! Ne on nyt tallennettu!';
         $interval(clearMessage, 5000, 1);
