9dc79ea0d6889a21ca386a2b9f5bad16b90d8b83
niklas <niklas@niklas.niklas>
2016-08-05 18:42:06 +0300

    moved lot of chart things into chart factory

--- app/assets/javascripts/shared/chartJuttu.js
+++ app/assets/javascripts/shared/chartJuttu.js
@@ -43,6 +43,71 @@ angular.module('ocWebGui.shared.chartJuttu.service', [])
         }
       },
 
+      scatterOptions: {
+        chart: {
+          type: 'scatterChart',
+          width: 700,
+          height: 300,
+          margin: {
+            top: 30,
+            right: 90,
+            bottom: 60,
+            left: 90
+          },
+          color: d3.scale.category10().range(),
+          showDistX: true,
+          showDistY: true,
+          xAxis: {
+            axisLabel: 'Kellonaika',
+            tickFormat: function (d) {
+              return d3.time.format('%H.%M')(new Date(d));
+            }
+          },
+          yAxis: {
+            axisLabel: 'Jonotusaika',
+            axisLabelDistance: 10,
+            tickFormat: function (seconds) {
+              var formatTime = d3.time.format('%H:%M');
+              return formatTime(new Date(1864, 7, 7, 0, seconds));
+            }
+          },
+          x: function (d) { return d.hour; },
+          y: function (d) { return d.calls; }
+        }
+      },
+
+      multiChartOptions: {
+        chart: {
+          type: 'multiChart',
+          width: 700,
+          height: 550,
+          margin: {
+            top: 30,
+            right: 90,
+            bottom: 60,
+            left: 90
+          },
+          x: function (d) { return d.hour; },
+          y: function (d) { return d.calls; },
+          duration: 500,
+          xAxis: {
+            axisLabel: 'Kellonaika'
+          },
+          yAxis1: {
+            axisLabel: 'Jonottajat',
+            yDomain: [0, 10]
+          },
+          yAxis2: {
+            axisLabel: 'Aikaa',
+            tickFormat: function (seconds) {
+              var formatTime = d3.time.format('%H:%M');
+              return formatTime(new Date(1864, 7, 7, 0, seconds));
+            },
+            yDomain: [0, 10]
+          }
+        }
+      },
+
       mapAndFilter: function (data, settings) {
         return data
           .map(function (calls, hour) { return { hour: hour, calls: calls }; })
