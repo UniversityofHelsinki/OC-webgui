a45f0385924dfee6d68af1b7b7fbbf3b3061c4fc
niklas <niklas@niklas.niklas>
2016-08-09 19:53:00 +0300

    add mock to timeDirective.spec.js

--- app/assets/javascripts/shared/time/timeDirective.spec.js
+++ app/assets/javascripts/shared/time/timeDirective.spec.js
@@ -12,6 +12,22 @@ describe('ocTime', function () {
         return {
           getDate: function () {
             return currentDate;
+          },
+
+          niceFormatting: function (currentSeconds) {
+            var pad2 = function (value) {
+              return (value < 10 ? '0' : '') + value;
+            };
+            var seconds = Math.floor(currentSeconds % 60);
+            var minutes = Math.floor(currentSeconds / 60);
+
+            if (minutes > 60) {
+              minutes = Math.floor(currentSeconds / 60 % 60);
+              var hours = Math.floor(currentSeconds / 60 / 60);
+              return pad2(hours) + ':' + pad2(minutes) + ':' + pad2(seconds);
+            }
+
+            return pad2(minutes) + ':' + pad2(seconds);
           }
         };
       });
