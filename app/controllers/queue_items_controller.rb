78964b711d86a2ef8dd3fe11c516807fcbabb8a5
Tuomas Siipola <siiptuo@kapsi.fi>
2016-08-11 11:47:14 +0300

    Add raw query parameter in queue items API for debugging
    Raw option simply fetches and prints the raw data from SOAP API
    bypassing cache and all formatting.

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,6 +1,7 @@
 # API for Queue status data
 class QueueItemsController < ApplicationController
   def index
+    return render json: BackendService.new.get_general_queue if params['raw']
     return render json: [] if Rails.cache.read('queue_items').nil?
     render json: Rails.cache.read('queue_items')
   end
