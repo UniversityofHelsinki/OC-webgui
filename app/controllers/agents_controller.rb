b66270272f9c6d6bbe8f120f31acb324a4894493
niklas <niklas@niklas.niklas>
2016-08-14 15:47:57 +0300

    refresh users database when querying agents.json

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,6 +1,10 @@
 # API for Agents data
 class AgentsController < ApplicationController
   def index
+    BackendService.new.get_agents.map do |data|
+      name = [data[:first_name], data[:last_name]].join(' ')
+      Agent.find_or_create(data[:agent_id], name, data[:team])
+    end
     @agents = Agent.all
   end
 end
