f3027aab31f07f278216fef578d3ad3cb97f0204
niklas <niklas@niklas.niklas>
2016-08-15 21:39:43 +0300

    add test for soap agent fetch

--- spec/controllers/agents_controller_spec.rb
+++ spec/controllers/agents_controller_spec.rb
@@ -19,4 +19,41 @@ RSpec.describe AgentsController, type: :controller do
     expect(JSON.parse(response.body)).to include(agent1)
     expect(JSON.parse(response.body)).to include(agent2)
   end
+
+  it "returns all current agents in index and new found from soap" do
+    agents_map = [{:agent_id => 1,
+                   :last_name => "x",
+                   :first_name => "keke",
+                   :team_name => "Helpdesk"},
+                  {:agent_id => 2,
+                   :last_name => "x",
+                   :first_name => "spede",
+                   :team_name => "Helpdesk"},
+                  {:agent_id => 3,
+                   :last_name => "x",
+                   :first_name => "harri",
+                   :team_name => "Puhelinvaihde"}]
+    allow_any_instance_of(BackEndService).to receive(:get_agents).and_return(nil)
+    Agent.create(id: 1, first_name: "keke", last_name: "x")
+    Agent.create(id: 2, first_name: "spede", last_name: "x")
+    get :index, format: :json
+    agent1 = {
+      "id"=>1,
+      "first_name"=>"keke",
+      "last_name"=>"x",
+    }
+    agent2 = {
+      "id"=>2,
+      "first_name"=>"spede",
+      "last_name"=>"x"
+    }
+    agent3 = {
+      "id"=>3,
+      "first_name"=>"harri",
+      "last_name"=>"x",
+    }
+    expect(JSON.parse(response.body)).to include(agent1)
+    expect(JSON.parse(response.body)).to include(agent2)
+    expect(JSON.parse(response.body)).to include(agent3)
+  end
 end
