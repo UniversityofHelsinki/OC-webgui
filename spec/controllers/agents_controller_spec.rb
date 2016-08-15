eede5ec1c0149c29aa3843a7a55edefae7607e27
niklas <niklas@niklas.niklas>
2016-08-15 21:31:10 +0300

    change up

--- spec/controllers/agents_controller_spec.rb
+++ spec/controllers/agents_controller_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe AgentsController, type: :controller do
   render_views
 
   it "returns all current agents in index" do
+    allow_any_instance_of(AgentsController).to receive(:get_all).and_return(nil)
     Agent.create(id: 1, first_name: "keke", last_name: "x")
     Agent.create(id: 2, first_name: "spede", last_name: "x")
     get :index, format: :json
@@ -15,7 +16,6 @@ RSpec.describe AgentsController, type: :controller do
       "first_name"=>"spede",
       "last_name"=>"x"
     }
-    allow_any_instance_of(AgentsController).to receive(:get_all).and_return(nil)
     expect(JSON.parse(response.body)).to include(agent1)
     expect(JSON.parse(response.body)).to include(agent2)
   end
