43fad8dcf2b465a3608cefa234a29eaf409c27b0
niklas <niklas@niklas.niklas>
2016-08-16 21:41:57 +0300

    team_name not team

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -8,7 +8,7 @@ class AgentsController < ApplicationController
   def get_all
     BackendService.new.get_agents.map do |data|
       name = [data[:last_name], data[:first_name]].join(' ')
-      Agent.find_or_create(data[:agent_id], name, data[:team])
+      Agent.find_or_create(data[:agent_id], name, data[:team_name])
     end
   end
 end
