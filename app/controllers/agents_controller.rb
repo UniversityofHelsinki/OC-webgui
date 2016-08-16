08908c4c6981e7bdc425f842b477f2a79b222e1c
niklas <niklas@niklas.niklas>
2016-08-16 21:43:52 +0300

    add comment

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,11 +1,12 @@
 # API for Agents data
 class AgentsController < ApplicationController
   def index
-    get_all
+    fetch_and_save_all_agents
     @agents = Agent.all
   end
 
-  def get_all
+  # Fetches all agents from SOAP and saves them to database
+  def fetch_and_save_all_agents
     BackendService.new.get_agents.map do |data|
       name = [data[:last_name], data[:first_name]].join(' ')
       Agent.find_or_create(data[:agent_id], name, data[:team_name])
