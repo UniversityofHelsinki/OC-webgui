45bca7a6c142cfa0ea1a084527c79e540f1cbeef
niklas <niklas@niklas.niklas>
2016-08-17 15:41:38 +0300

    forgot semicolon

--- app/assets/javascripts/stats/status/statusService.js
+++ app/assets/javascripts/stats/status/statusService.js
@@ -4,7 +4,7 @@ angular.module('ocWebGui.stats.status.service', ['ngResource'])
       stats: function (startDate, endDate, reportType) {
         // swap dates other way around if startDate is after endDate
         if (startDate > endDate) {
-          [startDate, endDate] = [endDate, startDate]
+          [startDate, endDate] = [endDate, startDate];
         }
         return $http.post('agent_statuses/stats', {
           report_type: reportType,
